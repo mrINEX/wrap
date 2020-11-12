ab65b06e9275288a48976d24631469341b143d7c
mrinex <mrinex@mail.ru>
2020-11-12 22:54:39 +0300

    feat: add DOM elements

---
+++ dist/bundle.js
@@ -0,0 +1,122 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("var wrapSquares = [{\n  quantity: 10,\n  classColor: 'beige'\n}, {\n  quantity: 10,\n  classColor: 'letters-are-blue'\n}, {\n  quantity: 4,\n  classColor: 'yellow'\n}, {\n  quantity: 16,\n  classColor: 'snake'\n}, {\n  quantity: 4,\n  classColor: 'brown-flowers'\n}, {\n  quantity: 8,\n  classColor: 'red-flowers'\n}, {\n  quantity: 17,\n  classColor: 'red'\n}, {\n  quantity: 10,\n  classColor: 'citric'\n}, {\n  quantity: 2,\n  classColor: 'light-yellow'\n}, {\n  quantity: 10,\n  classColor: 'light-gray'\n}, {\n  quantity: 8,\n  classColor: 'gray'\n}, {\n  quantity: 8,\n  classColor: 'dark-green'\n}, {\n  quantity: 4,\n  classColor: 'dark-blue'\n}];\n\nwindow.onload = function () {\n  console.log('Hello Volha =)');\n  var containerWrap = document.createElement('div');\n  containerWrap.classList.add('container-wrap'); // collection area\n\n  var wrapperCollectionArea = document.createElement('div');\n  var collectingArea = document.createElement('div');\n  collectingArea.classList.add('collecting-area');\n  Array(108).fill(' ').map(function () {\n    var collectSquare = document.createElement('div');\n    collectSquare.classList.add('collect-square');\n    collectingArea.append(collectSquare);\n  }); // squares area\n\n  var squareArea = document.createElement('div');\n  squareArea.classList.add('square-area');\n  wrapSquares.map(function (_ref) {\n    var quantity = _ref.quantity,\n        classColor = _ref.classColor;\n    Array(quantity).fill(' ').forEach(function () {\n      var square = document.createElement('div');\n      square.classList.add(classColor, 'square');\n      squareArea.append(square);\n    });\n  });\n  wrapperCollectionArea.append(collectingArea);\n  containerWrap.append(wrapperCollectionArea, squareArea);\n  document.querySelector('body').append(containerWrap);\n};\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/scss/style.scss":
+/*!*****************************!*\
+  !*** ./src/scss/style.scss ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/scss/style.scss?");
+
+/***/ }),
+
+/***/ 0:
+/*!**************************************************!*\
+  !*** multi ./src/index.js ./src/scss/style.scss ***!
+  \**************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("__webpack_require__(/*! ./src/index.js */\"./src/index.js\");\nmodule.exports = __webpack_require__(/*! ./src/scss/style.scss */\"./src/scss/style.scss\");\n\n\n//# sourceURL=webpack:///multi_./src/index.js_./src/scss/style.scss?");
+
+/***/ })
+
+/******/ });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,89 @@
+const wrapSquares = [
+  {
+    quantity: 10,
+    classColor: 'beige'
+  },
+  {
+    quantity: 10,
+    classColor: 'letters-are-blue'
+  },
+  {
+    quantity: 4,
+    classColor: 'yellow'
+  },
+  {
+    quantity: 16,
+    classColor: 'snake'
+  },
+  {
+    quantity: 4,
+    classColor: 'brown-flowers'
+  },
+  {
+    quantity: 8,
+    classColor: 'red-flowers'
+  },
+  {
+    quantity: 17,
+    classColor: 'red'
+  },
+  {
+    quantity: 10,
+    classColor: 'citric'
+  },
+  {
+    quantity: 2,
+    classColor: 'light-yellow'
+  },
+  {
+    quantity: 10,
+    classColor: 'light-gray'
+  },
+  {
+    quantity: 8,
+    classColor: 'gray'
+  },
+  {
+    quantity: 8,
+    classColor: 'dark-green'
+  },
+  {
+    quantity: 4,
+    classColor: 'dark-blue'
+  },
+];
+
 window.onload = function() {
   console.log('Hello Volha =)');
+
+  const containerWrap = document.createElement('div');
+  containerWrap.classList.add('container-wrap');
+
+  // collection area
+  const wrapperCollectionArea = document.createElement('div');
+
+  const collectingArea = document.createElement('div');
+  collectingArea.classList.add('collecting-area');
+
+  Array(108).fill(' ').map(() => {
+    const collectSquare = document.createElement('div');
+    collectSquare.classList.add('collect-square');
+    collectingArea.append(collectSquare);
+  });
+
+  // squares area
+  const squareArea = document.createElement('div');
+  squareArea.classList.add('square-area');
+
+  wrapSquares.map(({ quantity, classColor }) => {
+    Array(quantity).fill(' ').forEach(() => {
+      const square = document.createElement('div');
+      square.classList.add(classColor, 'square');
+      squareArea.append(square);
+    });
+  });
+
+  wrapperCollectionArea.append(collectingArea);
+  containerWrap.append(wrapperCollectionArea, squareArea);
+  document.querySelector('body').append(containerWrap);
 }
